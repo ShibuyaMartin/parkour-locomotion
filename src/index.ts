470c9b5ef3532cabb7dcff059b01a2d815c71c17
Shibu <shibu@decentraland.org>
2023-09-11 10:55:23 -0300

    add base plaza

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { engine, executeTask, GltfContainer, Material, Transform } from '@dcl/sdk/ecs'
-import { Color4, Vector3 } from '@dcl/sdk/math'
+import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
 export * from '@dcl/sdk'
 export function main() {
  
@@ -7,11 +7,12 @@ export function main() {
 
 let samplescene = engine.addEntity()
 
-GltfContainer.create(samplescene, {src: "models/parkour-scene.glb"})
+GltfContainer.create(samplescene, {src: "models/base-plaza.glb"})
 
 Transform.create(samplescene, {
-	  position:  Vector3.create(48, 0, 48),
-	  scale: Vector3.create(1, 1, 1)
+	  position:  Vector3.create(0, 0, 0),
+	  scale: Vector3.create(1, 1, 1),
+	  rotation: Quaternion.fromEulerDegrees(0, 180, 0)
 	})
 
   let samplescene_02 = engine.addEntity()
